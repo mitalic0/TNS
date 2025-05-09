5e929d4356a77ae1db01d50dbc03534f24264518
MitaliC <cmitali330@gmail.com>
2025-05-09 10:21:16 +0530

    Add files via upload

---
+++ Assignment-1/com/yourname/assignment/employees/Developer.java
@@ -0,0 +1,13 @@
+package com.yourname.assignment.employees;
+
+public class Developer extends Employee {
+    private String programmingLanguage;
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+    
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+}
---
+++ Assignment-1/com/yourname/assignment/employees/Manager.java
@@ -0,0 +1,13 @@
+package com.yourname.assignment.employees;
+
+public class Manager extends Employee {
+    private String department;
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
